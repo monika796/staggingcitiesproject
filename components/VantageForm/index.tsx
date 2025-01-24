263855e67d5a68055349a3447a06274c4b27b371
monika796 <monikagupta796@gmail.com>
2025-01-24 14:17:56 +0530

    aaaaa

--- components/VantageForm/index.tsx
+++ components/VantageForm/index.tsx
@@ -45,7 +45,7 @@ const SecondSection = ({ pdfData }) => {
 const handleDownload = async (url: string) => {
   setLoading(true);
   try {
-    const response = await fetch(url, { mode: 'no-cors' });
+    const response = await fetch(url);
     if (!response.ok) {
       throw new Error(`Failed to fetch the PDF file. Status: ${response.status}`);
     }
