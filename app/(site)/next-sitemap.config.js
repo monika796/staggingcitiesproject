a872b8a25b4fe6d5d6a9010032bb2cd4ddc326fe
monikagupta <monikagupta796@gmail.com>
2025-02-04 12:15:52 +0530

    done

---
+++ app/(site)/next-sitemap.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    siteUrl: process.env.NEXT_PUBLIC_SITEMAP_URL || "https://example.com",
+    generateRobotsTxt: true, // Generate robots.txt as well
+    sitemapSize: 5000,
+};
