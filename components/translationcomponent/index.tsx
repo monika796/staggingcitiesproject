34ac800d13aaba63defbce40e5db67353cc1b8ae
monikagupta <monikagupta796@gmail.com>
2025-02-26 15:50:59 +0530

    testtt

--- components/translationcomponent/index.tsx
+++ components/translationcomponent/index.tsx
@@ -4,17 +4,23 @@ import { useRouter } from "next/navigation";
 const LanguageSwitcher = () => {
   const router = useRouter();
 
-  const changeLanguage = (lang) => {
+  const changeLanguage = (lang: string) => {
+    console.log(lang)
     document.cookie = `lang=${lang}; path=/; max-age=31536000`; // Save language in cookie
     router.refresh(); // Refresh page to load translations
   };
 
   return (
-    <div className="flex justify-center items-center gap-2"> 
-      <button onClick={() => changeLanguage("en")}> English</button>
-      <button onClick={() => changeLanguage("fr")}> French</button>
-      <button onClick={() => changeLanguage("es")}>Spanish</button>
-      <button onClick={() => changeLanguage("ru")}>Russian</button>
+    <div className="flex justify-center items-center">
+      <select
+        onChange={(e) => changeLanguage(e.target.value)}
+        className="border rounded-md px-3 py-2"
+      >
+        <option value="en">English</option>
+        <option value="fr">French</option>
+        <option value="es">Spanish</option>
+        <option value="ru">Russian</option>
+      </select>
     </div>
   );
 };
