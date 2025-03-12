c3f02994db37855a91530817414ed02abd4587fa
monika796 <132574582+monika796@users.noreply.github.com>
2025-03-12 13:29:13 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -38,7 +38,7 @@ const nextConfig = {
   },
   reactStrictMode: false,
   images: {
-    domains: ["localhost", "backend.citiesprojectglobal.com",'cityglobalprojects.insiderwebsolutions.com'], // Trusted domains for images
+    domains: ["localhost", "backend.citiesprojectglobal.com","cityglobalprojects.insiderwebsolutions.com"], // Trusted domains for images
   },
   env: {
     NEXT_STRIPE_PUBLISH_KEY: process.env.NEXT_STRIPE_PUBLISH_KEY,
